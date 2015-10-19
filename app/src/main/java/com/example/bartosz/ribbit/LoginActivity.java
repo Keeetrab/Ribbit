58323e72e6cfbc8cea48854a5257d915e376bfe4
Bartosz Kosewski <bartekkosewski@gmail.com>
2015-10-19 15:31:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bartosz/ribbit/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.bartosz.ribbit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.signUpText) TextView mSignUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.signUpText)
+    public void goToSignUp() {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
